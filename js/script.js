efdd9ff6a0fdff2b3aa0a8cce19d19305794bd90
Thiago Gaitkoski <68345301+ThiagoGaitkoski@users.noreply.github.com>
2021-11-02 19:15:45 -0300

    Adição de controle das áreas da tela

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let seuVotoPara = document.querySelector('.voto span');
+let cargo = document.querySelector('.voto-candidato span');
+let votoInfo = document.querySelector('.voto-info');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let digito = document.querySelector('.voto-digitos')
